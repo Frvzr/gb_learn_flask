b4f3f7b57a46693d9a8db524688459af9557f658
Ivan Koposhilov <ikoposhilov@yandex.ru>
2023-03-05 12:05:21 +0500

    lesson_04 completed

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,22 +1,18 @@
 from flask import Blueprint, render_template
 from werkzeug.exceptions import NotFound
+from blog.models.models import Article
 
 
 articles_app = Blueprint("articles_app", __name__, url_prefix='/articles', static_folder='../static')
 
-ARTICLES = {
-    1: "Flask", 
-    2: "Django", 
-    3: "JSON:API"}
-
 @articles_app.route("/", endpoint="list")
 def articles_list():
-    return render_template("articles/list.html", articles_app=ARTICLES)
+    articles = Article.query.all()
+    return render_template("articles/list.html", articles=articles)
 
 @articles_app.route("/<int:pk>/")
 def get_articles(pk: int):
-    try:
-        articles_name = ARTICLES[pk]
-    except KeyError:
-        raise NotFound(f"Articles {pk} doesn't exists")
-    return render_template('articles/details.html', articles_id=pk, articles_name=articles_name)
+    article = Article.query.filter_by(article_id=pk).one_or_none()
+    if article is None:
+        raise NotFound(f"Article #{pk} doesn't exist!")
+    return render_template('articles/details.html', articles_app=article)
